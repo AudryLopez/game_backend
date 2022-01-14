016e5d3e0bfbdbd4aaa41425ebfad3bf838636e1
AudryLopez <alopez@intellisys.com.do>
2022-01-13 22:36:40 -0400

    conection in typeorm

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,16 @@
-import { createConnection, Connection } from "typeorm";
+import { createConnection } from "typeorm";
 
-const connection = await createConnection({
-  type: "mysql",
+createConnection({
+  type: "postgres",
   host: "localhost",
   port: 3306,
-  username: "test",
-  password: "test",
+  username: "admin",
+  password: "admin",
   database: "test",
-});
+  entities: [__dirname + "/src/modules/**/*.ts"],
+  synchronize: true,
+})
+  .then((connection) => {
+    // here you can start to work with your entities
+  })
+  .catch((error) => console.log(error));
