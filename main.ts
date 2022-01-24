441be85f11b66dd9e495e45ef2d770d3be39115b
AudryLopez <alopez@intellisys.com.do>
2022-01-24 13:10:19 -0400

    change in user modle

--- main.ts
+++ main.ts
@@ -4,9 +4,15 @@ import router from "./src/routes/routes";
 
 const app = express();
 const port = 3000;
+//const swaggerUi = require("swagger-ui-express");
+//const swaggerDocument = require("./swagger.json");
 
 app.use(cors());
 app.use(express.json());
+
+
+//router.use("/api-docs", swaggerUi.serve);
+//router.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 app.listen(port, () => {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/home", ()=>{
-    return "<h1>Hola</h1>"
+router.get("/", (req, res)=>{
+   res.send("<h1>Hola</h1>") 
 })
 
 export default router;
